d704f8f7c5d974f4dddd86be92aec05cf68ef952
Kieran Sullivan <kms1241@rit.edu>
2024-10-23 17:45:12 -0400

    importUsers function finished

--- dataLayer.js
+++ dataLayer.js
@@ -1,12 +1,14 @@
 const { Client } = require('pg');
+const fs = require('fs'); //file system module
+
 let CLIENT;
 
 function newConnection(uname, hname, db, pwd,){ //given the proper string paramaters, creates connection to db
      CLIENT = new Client({
-        user: uname,
-        host: hname, 
-        database: db,
-        password: pwd,
+        user: uname, //adminuser
+        host: hname, //localhost
+        database: db, //americandream[db]
+        password: pwd, //adminpass
         port: 5432
     });
     CLIENT.connect();
@@ -34,6 +36,27 @@ function getUserData(uname, pwd) {
     })
 
 }
+
+async function importUsers(filename){
+    const data = fs.readFileSync(filename, 'utf8');
+    const lines = data.split('\n');
+
+    for (let line of lines) {
+        let fields = line.split('|');
+        
+        let [u_id, fname, lname, uname, s_id, u_role] = fields;
+
+        const query = `SELECT add_user($1, $2, $3, $4, $5, $6);`;
+
+        try {
+            await CLIENT.query(query, fields);
+        } catch (error) {
+            console.error('Error adding user:', error);
+        }
+        
+    }
+}
+importUsers('./data/members.psv');
 function termconn(){
     CLIENT.end();
 }
