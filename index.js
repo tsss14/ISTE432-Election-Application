e97b019cf46f1f6d236533221b9bee22ade99a96
Alex T <52086722+tsss14@users.noreply.github.com>
2024-11-04 12:46:57 -0500

    added create user endpoint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const {validateLogin} = require('./businessLayer.js');
+const {validateLogin, createUser} = require('./businessLayer.js');
 const port = 3000;
 
 app.use((req, res, next) => {
@@ -18,6 +18,20 @@ app.post("/login", async function(req, res) {
         const returnVal = await validateLogin(uname, pwrd);
         if(returnVal === "") {
             return res.status(400).send("Bad password...");
+        }
+        return res.status(200).send(returnVal);
+});
+
+app.post("/usrcreate", async function(req, res) {
+	console.log(req.body);
+	const uname = req.body.username;
+	const role = req.body.role;
+	const fname = req.body.fname;
+	const lname = req.body.lname;
+	const phone = req.body.phone;
+        const returnVal = await createUser(uname, role, fname, lname, phone);
+        if(returnVal === "") {
+            return res.status(400).send("Bad user info...");
         }
         return res.status(200).send(returnVal);
 });
