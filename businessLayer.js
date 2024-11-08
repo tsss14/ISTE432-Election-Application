08d9f07ac615000b599aec70016721b80028a05a
Alex T <52086722+tsss14@users.noreply.github.com>
2024-11-08 15:39:02 -0500

    added first draft of create ballot function

--- businessLayer.js
+++ businessLayer.js
@@ -32,10 +32,10 @@ async function validateLogin(username, password) {
 
 async function createUser(uname, role, fname, lname, phone) {
     console.log("Attempting user creation...");
-    if(checkInput(uname) &&
-    	checkInput(role) &&
-    	checkInput(fname) &&
-    	checkInput(lname) &&
+    if(checkInput(uname) ||
+    	checkInput(role) ||
+    	checkInput(fname) ||
+    	checkInput(lname) ||
     	checkInput(phone)
     ) {
         return "";
@@ -53,10 +53,8 @@ async function createUser(uname, role, fname, lname, phone) {
 async function createSociety(socName) {
     console.log("Attempting society creation...");
     if(checkInput(socName)) {
-    	console.log(checkInput(socName))
         return "";
     } else {
-    	console.log(checkInput(socName))
         console.log("Acceptable field values... Querying database...")
         const socAdded = await addSociety(socName);
         if(socAdded) {
@@ -67,4 +65,27 @@ async function createSociety(socName) {
     }
 }
 
-module.exports = { validateLogin, createUser, createSociety };
+async function createBallot(socName, elecName, cndts, inits, start, durr, desc) {
+    console.log("Attempting ballot creation...");
+    if(checkInput(socName) ||
+    	checkInput(elecName) ||
+    	checkInput(cndts) ||
+    	checkInput(inits) ||
+        checkInput(start) ||
+        checkInput(durr) ||
+    	checkInput(desc)) {
+        return "";
+    } else {
+        console.log("Acceptable field values... Querying database...")
+	//Call a function multiple times to add each candidate and initiative
+	//Finally call a function to create the ballot itself 
+        //const ballotAdded = await addSociety(socName);
+        if(ballotAdded) {
+        	return true;
+        } else {
+        	return "";
+        }
+    }
+}
+
+module.exports = { validateLogin, createUser, createSociety, createBallot };
