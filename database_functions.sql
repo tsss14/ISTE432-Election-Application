820f2913108529300b3d2cd45fe19bf91036402c
Alex T <alt2643@rit.edu>
2024-11-13 11:03:46 -0500

    someones databaseFunciton changes

--- database_functions.sql
+++ database_functions.sql
@@ -1,40 +1,41 @@
-CREATE OR REPLACE FUNCTION get_user_login(
-    p_username VARCHAR,
-    p_password VARCHAR
-)
-RETURNS TABLE (
-    user_id INT,
-    name VARCHAR,
-    phone VARCHAR,
-    role VARCHAR,
-    hasVoted BOOLEAN
-) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT 
-        "User".user_id,
-        "User".first_name,
-        "User".phone,
-        "User".role,
-        "User"."hasVoted"
-    FROM 
-        americandreamdb."User"
-    WHERE 
-        username = p_username 
-        AND password = p_password;
-END;
-$$ LANGUAGE plpgsql;
+--CREATE OR REPLACE FUNCTION get_user_login(
+--    p_username VARCHAR,
+--    p_password VARCHAR
+--)
+--RETURNS TABLE (
+--    user_id INT,
+--    name VARCHAR,
+--    phone VARCHAR,
+--    role VARCHAR,
+--    hasVoted BOOLEAN
+--) AS $$
+--BEGIN
+--    RETURN QUERY
+--    SELECT 
+--        "User".user_id,
+--        "User".first_name,
+--        "User".phone,
+--        "User".role,
+--        "User"."hasVoted"
+--    FROM 
+--        americandreamdb."User"
+--    WHERE 
+--        username = p_username 
+--        AND password = p_password;
+--END;
+--$$ LANGUAGE plpgsql;
 
 -- TEST DATA ENTRY 
 -- Create the Society
-INSERT INTO americanDreamDB."Society" ("name", "memberCount", "avgVote")
-VALUES ('Test1', 0, 0.0)
+INSERT INTO americanDreamDB."Society" ("society_id", "name", "memberCount", "avgVote")
+VALUES (60,'Test1', 0, 0.0)
 RETURNING "society_id";
 
 -- Create the Election assigned to the Society
-INSERT INTO americanDreamDB."Election" ("society_id", "name", "totalVotes", "ballotCount", "activity", "startsAt", "endsAt")
+INSERT INTO americanDreamDB."Election" ("election_id", "society_id", "name", "totalVotes", "ballotCount", "activity", "startsAt", "endsAt")
 VALUES (
-    (SELECT "society_id" FROM americanDreamDB."Society" WHERE "name" = 'Test1'),
+    50,
+    60,
     'ElectionTest1', 
     0, 0, TRUE, 
     '2024-11-12', '2024-12-12'
@@ -42,9 +43,10 @@ VALUES (
 RETURNING "election_id";
 
 -- Create the Office for the Election
-INSERT INTO americanDreamDB."Office" ("election_id", "officeName", "officeVotesAllowed")
+INSERT INTO americanDreamDB."Office" ("office_id", "election_id", "officeName", "officeVotesAllowed")
 VALUES (
-    (SELECT "election_id" FROM americanDreamDB."Election" WHERE "name" = 'ElectionTest1'),
+    40,
+    50,
     'OfficeTest1', 1
 )
 RETURNING "office_id";
