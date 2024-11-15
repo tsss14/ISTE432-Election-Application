47e7a632ea82d13c89ffc6b57f5130f9362f8816
Kieran Sullivan <kms1241@rit.edu>
2024-11-15 13:57:20 -0500

    prevElection Datafetch refactor

--- pastElections/pastElections.js
+++ pastElections/pastElections.js
@@ -1,43 +1,38 @@
 
 async function dataLoad() {
-  const response = await fetch('http://localhost:3000/pastElections');
-  if(!response.ok) {
-    throw new Error('couldnt fetch data');
-  }
-  const data = await response.json();
-  console.log(data);
-} 
-
-async function init() {
-    const header = $('<div>');
-    const placeholderName = "placeholder name";
-    const data = await dataLoad();
-    data.json();
-    console.log(data);
-
-    
-    $(document.body).append(header);
+  $(document.body).append(header);
     $(header).addClass("text-bg-primary p-3").text("Welcome!");
     $(document.body).append(`<form>\
         <h2>Previous Elections</h2>`);
+  const response = await fetch('http://localhost:3000/pastElections')
+    .then(response => response.json())
+    .catch(err => console.log("Couldn't fetch data: ", err));
+  if(response && response.endsAt) {
+    displayElections(response);
+  }else  {
+    $('#electionContent').html('<h4>No previous elections found.</h4>')
+  }
+} 
 
-        data.forEach(election => {
-          const elecDiv = document.createElement('div');
-          elecDiv.id = "elecDiv";
+function displayElections(data) {
+
+  data.forEach(election => {
+    const elecDiv = document.createElement('div');
+    elecDiv.id = "elecDiv";
         
-          const elecTitle = document.createElement('div');
-          elecTitle.id = "elecTitle";
-          elecTitle.textContent = data.title;
+    const elecTitle = document.createElement('div');
+    elecTitle.id = "elecTitle";
+    elecTitle.textContent = data.title;
         
-          const endDate = document.createElement('div');
-          endDate.id = "endDate";
-          endDate.textContent = data.endDate;
+    const endDate = document.createElement('div');
+    endDate.id = "endDate";
+    endDate.textContent = data.endDate;
         
-          elecDiv.appendChild(elecTitle);
-          elecDiv.appendChild(endDate);
+    elecDiv.appendChild(elecTitle);
+    elecDiv.appendChild(endDate);
         
-          body.appendChild(elecDiv);
-});
+    $('#electionContent').appendChild(elecDiv);
+  });
 }
 
-$(document).ready(init);
+$(document).ready(dataLoad);
