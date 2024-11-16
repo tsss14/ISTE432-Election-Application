8b0918aec4a778ed7dff5204c2b87eab6278e2ab
Kieran Sullivan <kms1241@rit.edu>
2024-11-16 18:05:59 -0500

    ongoing changes

--- onGoingElections/ongoing.js
+++ onGoingElections/ongoing.js
@@ -1,15 +1,11 @@
 async function dataLoad() {
-  $(document.body).append(header);
-    $(header).addClass("text-bg-primary p-3").text("Welcome!");
-    $(document.body).append(`<form>\
-        <h2>Ongoing Elections</h2>`);
   const response = await fetch('http://localhost:3000/ongoingElections')
     .then(response => response.json())
     .catch(err => console.log("Couldn't fetch data: ", err));
   if(response && response.endsAt) {
     displayElections(response);
   }else  {
-    $('#electionContent').html('<h4>No previous elections found.</h4>')
+    $('#electionContent').html('<h4>No ongoing elections found.</h4>')
   }
 } 
 
