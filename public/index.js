63365142cc7997041a633b3aa14e9b9d893b8b05
Alex T <alt26432@rit.edu>
2024-11-22 15:00:27 -0500

    working localhost page serving

--- public/index.js
+++ public/index.js
@@ -3,12 +3,15 @@ const app = express();
 const {validateLogin, createOffice, createBallot, createUser, createSociety, callPreviousElections, callOngoingElections, callElection,callSocieties, getActiveElectionByUser, getSystemStats, getElectionData, callProfile} = require('./businessLayer.js');
 const port = 3000;
 
-//app.use(express.static('/public/'));
+app.use('/static', express.static('public'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.type('application/javascript');
+
     next();
     // const start = Date.now(); This function need to be in this app.use, but the query should happen in the business layer
     // res.on('finish', () => {
