a2ad16767172434f6658c640ff9fd62c8ae6e3fa
Luke Doherty <127516396+ldoherty13@users.noreply.github.com>
2024-12-11 05:49:49 -0500

    Update memberMenu.js

--- public/memberMenu/memberMenu.js
+++ public/memberMenu/memberMenu.js
@@ -6,13 +6,14 @@ function init() {
     $(header).addClass("text-bg-primary p-3").text("Welcome! " + placeholderName);
     $(document.body).append('<div class="btn-group-vertical" role="group" aria-label="Vertical button group">');
     //$('div').last().append('<button type="button" class="btn btn-primary">Your Societies</button>');
-    $('div').last().append('<button type="button" id="ongoing" class="btn btn-primary">Ongoing Elections</button>');
+    $('div').last().append('<button type="button" id="current" class="btn btn-primary">Current Election</button>');
+    //$('div').last().append('<button type="button" id="ongoing" class="btn btn-primary">Ongoing Elections</button>');
     //$('div').last().append('<button type="button" class="btn btn-primary">Past Elections</button>');
     //$('div').last().append('<button type="button" class="btn btn-primary">Your Profile</button>');
     $('div').last().append('<button type="button" class="btn btn-primary">Logout</button>');
     $('div').last().addClass('position-absolute top-50 start-50 translate-middle');
-    $('#ongoing').on('click',function() {
-        window.location.href = '../onGoingElections/index.html';
+    $('#current').on('click',function() {
+        window.location.href = '../ballotView/index_test2.html';
     });
 }
 $(document).ready(init);
